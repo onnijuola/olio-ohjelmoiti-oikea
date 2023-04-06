afa7ef48f10cd024b9567aae5f1f79bd15a857ae
onnijuola <113601591+onnijuola@users.noreply.github.com>
2023-04-06 12:38:01 +0300

    esimerkki eläin valmi

---
+++ testi/testi/Program.cs
@@ -0,0 +1,25 @@
+﻿using testi;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        koira koiraolio = new koira();
+
+        koiraolio.nimi = "rekku";
+        koiraolio.ikä = 4;
+        koiraolio.väri = "harmaa";
+        koiraolio.paino = 13;
+        koiraolio.rotu = "saksan paimen";
+
+        koiraolio.hauku();
+
+
+        Console.WriteLine(koiraolio.haetiedot());
+
+
+    }
+
+   
+}
---
+++ testi/testi/koira.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testi
+{
+    internal class koira
+    {
+        public string nimi { get; set; }
+        public int ikä { get; set; }
+        public string väri { get; set; }
+        public float paino { get; set; }
+        public string rotu { get; set; }
+
+
+      
+
+        public void hauku()
+        {
+            Console.WriteLine("hau");
+
+        }
+
+        public string haetiedot()
+        {
+            string koirantiedot = "nimi: " + nimi + " ikä: " + ikä.ToString() + " väri: " + väri +" paino: " + paino.ToString() + " rotu: " + rotu;
+    
+
+                return koirantiedot;
+
+        }
+
+    }
+}
