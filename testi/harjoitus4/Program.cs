228be162934901e2b55cec3271999dcbcf3fa0ae
onnijuola <113601591+onnijuola@users.noreply.github.com>
2023-04-11 12:35:49 +0300

    harjoitus 3 ja 4 valmis

---
+++ testi/harjoitus4/Program.cs
@@ -0,0 +1,25 @@
+﻿using harjoitus4;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        kappale kappaleolio = new kappale("Nevermind", "Nirvana", 1991);
+
+        Kappale kappale1 = new Kappale("Smells Like Teen Spirit", "Nirvana", 302);
+        Kappale kappale2 = new Kappale("In Bloom", "Nirvana", 254);
+        Kappale kappale3 = new Kappale("Come As You Are", "Nirvana", 219);
+        Kappale kappale4 = new Kappale("Breed", "Nirvana", 183);
+        Kappale kappale5 = new Kappale("Lithium", "Nirvana", 257);
+
+        kappaleolio.LisaaKappale(kappale1);
+        kappaleolio.LisaaKappale(kappale2);
+        kappaleolio.LisaaKappale(kappale3);
+        kappaleolio.LisaaKappale(kappale4);
+        kappaleolio.LisaaKappale(kappale5);
+
+        kappaleolio.TulostaAlbumi();
+
+        Console.ReadKey();
+    }
+}
---
+++ testi/harjoitus4/kappale.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus4
+{
+    internal class kappale
+    {
+        public string Nimi { get; set; }
+        public string Artisti { get; set; }
+        public int Julkaisuvuosi { get; set; }
+        public List<Kappale> Kappaleet { get; set; }
+
+        public kappale(string nimi, string artisti, int julkaisuvuosi)
+        {
+            Nimi = nimi;
+            Artisti = artisti;
+            Julkaisuvuosi = julkaisuvuosi;
+            Kappaleet = new List<Kappale>();
+        }
+
+        public void LisaaKappale(Kappale kappale)
+        {
+            Kappaleet.Add(kappale);
+        }
+
+        public void TulostaAlbumi()
+        {
+            Console.WriteLine("Albumin tiedot:");
+            Console.WriteLine("Nimi: " + Nimi);
+            Console.WriteLine("Artisti: " + Artisti);
+            Console.WriteLine("Julkaisuvuosi: " + Julkaisuvuosi);
+            Console.WriteLine("Kappaleet:");
+
+            foreach (Kappale kappale in Kappaleet)
+            {
+                Console.WriteLine("- " + kappale.ToString());
+            }
+        }
+    }
+
+    class Kappale
+    {
+        public string Nimi { get; set; }
+        public string Esittaja { get; set; }
+        public int Kesto { get; set; }
+
+        public Kappale(string nimi, string esittaja, int kesto)
+        {
+            Nimi = nimi;
+            Esittaja = esittaja;
+            Kesto = kesto;
+        }
+
+        public override string ToString()
+        {
+            return Nimi + " - " + Esittaja + " (" + Kesto + " s)";
+        }
+    }
+}
