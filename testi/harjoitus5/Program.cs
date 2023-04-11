275feaaac852c8455d8836f85bdd4361bbaabc91
onnijuola <113601591+onnijuola@users.noreply.github.com>
2023-04-11 13:51:28 +0300

    harjoitus5 valmis

--- testi/harjoitus5/Program.cs
+++ testi/harjoitus5/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using harjoitus5;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        float a = 10;
+        float b = 5;
+        
+        Console.WriteLine("Summa: " + laskin.Summa(a, b));
+        Console.WriteLine("Erotus: " + laskin.Erotus(a, b));
+        Console.WriteLine("Kertolasku: " + laskin.Kertolasku(a, b));
+        Console.WriteLine("Jako: " + laskin.Jako(a, b));
     }
 }
---
+++ testi/harjoitus5/laskin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus5
+{
+    internal class laskin
+    {
+        public static float Summa(float a, float b)
+        {
+            return a + b;
+        }
+
+        public static float Erotus(float a, float b)
+        {
+            return a - b;
+        }
+
+        public static float Kertolasku(float a, float b)
+        {
+            return a * b;
+        }
+
+        public static float Jako(float a, float b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Jakaja ei voi olla nolla.");
+            }
+
+            return a / b;
+        }
+    }
+}
