0ec1b1514b286c65360a4b09430b178249906818
onnijuola <113601591+onnijuola@users.noreply.github.com>
2023-04-17 13:13:47 +0300

    harjoitus 7 valmis

---
+++ eläimet/harjoitus7/Program.cs
@@ -0,0 +1,12 @@
+﻿using harjoitus7;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        hissi hissiolio = new hissi();
+        Console.WriteLine("mihin kerrokseen haluat mennä? ");
+        Console.WriteLine(hissiolio.Kerros(7));
+        Console.WriteLine("hissin nopeus on " + hissiolio.Nopeus.ToString());
+    }
+}
---
+++ eläimet/harjoitus7/hissi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus7
+{
+    internal class hissi
+    {
+        private readonly int maxnopeus = 100;
+        private int nopeus;
+        public int Nopeus
+        {
+            get { return nopeus; }
+            set { nopeus = value; }
+        }
+
+
+        public int Kerros(int kerros)
+        {
+            kerros = Convert.ToInt32(Console.ReadLine());
+           
+            
+                if (kerros >= 1 && kerros <= 7)
+                { 
+                    Console.WriteLine("hissi on nyt kerroksella " + kerros);
+                }
+                else
+                {
+                    Console.WriteLine("error virheellinen ");
+                }
+            
+            return kerros;
+        }
+    }
+}
