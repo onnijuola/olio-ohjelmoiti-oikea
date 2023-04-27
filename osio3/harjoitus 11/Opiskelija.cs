70bdd4f968b319c86a2d466fae39112c80861032
onnijuola <113601591+onnijuola@users.noreply.github.com>
2023-04-27 11:58:46 +0300

    harjoitus 11 valmis

---
+++ osio3/harjoitus 11/Opiskelija.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus_11
+{
+    internal class Opiskelija
+    {
+        public string Etunimi { get; set; }
+        public string Sukunimi { get; set; }
+        public string Ryhmätunnus { get; set; }
+        public string OpiskelijaID { get; set; }
+
+        public Opiskelija(string etunimi, string sukunimi, string ryhmätunnus, string opiskelijaID)
+        {
+            Etunimi = etunimi;
+            Sukunimi = sukunimi;
+            Ryhmätunnus = ryhmätunnus;
+            OpiskelijaID = opiskelijaID;
+        }
+    }
+}
+
---
+++ osio3/harjoitus 11/Program.cs
@@ -0,0 +1,77 @@
+﻿using harjoitus_11;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+
+        Dictionary<string, Opiskelija> opiskelijat = new Dictionary<string, Opiskelija>();
+
+        while (true)
+        {
+            Console.Write("Komento (lisää/poista/tulosta/poistu): ");
+            string komento = Console.ReadLine();
+
+            if (komento == "lisää")
+            {
+                Console.Write("Etunimi: ");
+                string etunimi = Console.ReadLine();
+
+                Console.Write("Sukunimi: ");
+                string sukunimi = Console.ReadLine();
+
+                Console.Write("Ryhmätunnus: ");
+                string ryhmätunnus = Console.ReadLine();
+
+                Console.Write("OpiskelijaID: ");
+                string opiskelijaID = Console.ReadLine();
+
+                try
+                {
+                    Opiskelija opiskelija = new Opiskelija(etunimi, sukunimi, ryhmätunnus, opiskelijaID);
+                    opiskelijat.Add(opiskelijaID, opiskelija);
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("OpiskelijaID on jo käytössä!");
+                }
+            }
+            else if (komento == "poista")
+            {
+                Console.Write("OpiskelijaID: ");
+                string opiskelijaID = Console.ReadLine();
+
+                if (opiskelijat.Remove(opiskelijaID))
+                {
+                    Console.WriteLine("Opiskelija poistettu.");
+                }
+                else
+                {
+                    Console.WriteLine("Opiskelijaa ei löytynyt.");
+                }
+            }
+            else if (komento == "tulosta")
+            {
+                foreach (Opiskelija opiskelija in opiskelijat.Values)
+                {
+                    Console.WriteLine("{0} {1} ({2}), ID: {3}", opiskelija.Etunimi, opiskelija.Sukunimi, opiskelija.Ryhmätunnus, opiskelija.OpiskelijaID);
+                }
+            }
+            else if (komento == "poistu")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Tuntematon komento!");
+            }
+        }
+
+        Console.WriteLine("Ohjelma sulkeutuu.");
+    }
+
+
+
+}
