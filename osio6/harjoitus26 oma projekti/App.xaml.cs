bdd5e1e2105276241616902a288b7383090b8c09
onnijuola <113601591+onnijuola@users.noreply.github.com>
2023-06-01 11:55:54 +0300

    tehtävä 26 valmis

---
+++ osio6/harjoitus26 oma projekti/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace harjoitus26_oma_projekti
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ osio6/harjoitus26 oma projekti/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Option_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton selectedOption = sender as RadioButton;
+            if (selectedOption == option1)
+            {
+                displayImage.Source = new BitmapImage(new Uri(@"C:\Users\onni.juola\OneDrive - Careeria\Työpöytä\kuva1.jpg"));
+            }
+            else if (selectedOption == option2)
+            {
+                displayImage.Source = new BitmapImage(new Uri(@"C:\Users\onni.juola\OneDrive - Careeria\Työpöytä\kuva2.jpg"));
+            }
+            else if (selectedOption == option3)
+            {
+                displayImage.Source = new BitmapImage(new Uri(@"C:\Users\onni.juola\OneDrive - Careeria\Työpöytä\kuva3.jpg"));
+            }
+
+            displayImage.Visibility = Visibility.Visible;
+        }
+    }
+}
+
